f01e9999d015c3be95e64f1d75aaf880f84f0ab7
laidback/valen <valen.patel1@gmail.com>
2024-05-01 18:58:06 +0530

    first commit

---
+++ app/src/main/java/com/example/broadcastrecieverinandroid/AirplaneModeChangeReceiver.java
@@ -0,0 +1,23 @@
+package com.example.broadcastrecieverinandroid;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.widget.Toast;
+
+public class AirplaneModeChangeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // This method is called when the BroadcastReceiver receives a broadcast
+        // In this example, we'll display a toast message when the broadcast is received
+        if (isAirplaneModeOn(context.getApplicationContext())) {
+            Toast.makeText(context, "AirPlane mode is on", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "AirPlane mode is off", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private static boolean isAirplaneModeOn(Context context) {
+        return Settings.System.getInt(context.getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
+    }
+}
---
+++ app/src/main/java/com/example/broadcastrecieverinandroid/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.broadcastrecieverinandroid;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    //dynamic BR
+    AirplaneModeChangeReceiver airplaneModeChangeReceiver = new AirplaneModeChangeReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+//        airplaneModeChangeReceiver.onReceive(this,getIntent());
+
+        //Static BR
+        ImageView imageView = findViewById(R.id.imageView);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            imageView.setImageURI(intent.getParcelableExtra(Intent.EXTRA_STREAM));
+        }
+        //
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        registerReceiver(airplaneModeChangeReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(airplaneModeChangeReceiver);
+    }
+}
